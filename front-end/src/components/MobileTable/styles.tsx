651dc37423d44a9873ae67ff998b4fb827dcbe21
Carlos Antonio Sales de Oliveira Junior <carlosamtoni@gmail.com>
2024-07-26 18:02:30 -0300

    styles: adicionando margin-botton ao componente TableMobile

--- front-end/src/components/MobileTable/styles.tsx
+++ front-end/src/components/MobileTable/styles.tsx
@@ -5,6 +5,7 @@ export const TableElemt = styled.table`
   border-collapse: collapse;
   border-radius: 8px 8px 0px 0px;
   overflow: hidden; 
+  margin-bottom: 20px;
 `;
 
 export const Thead = styled.thead`
