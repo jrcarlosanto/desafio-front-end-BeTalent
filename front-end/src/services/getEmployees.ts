006ef0c83b0bd69ae387a8c91877235b043aa296
Carlos Antonio Sales de Oliveira Junior <carlosamtoni@gmail.com>
2024-07-26 16:29:37 -0300

    refactor: modificando a variavel de ambiente VITE_API_URL

--- front-end/src/services/getEmployees.ts
+++ front-end/src/services/getEmployees.ts
@@ -1,6 +1,4 @@
-const address = import.meta.env.VITE_API_ADDRESS || 'http://0.0.0.0';
-const port = import.meta.env.VITE_API_PORT || '3000';
-const url =  import.meta.env.VITE_API_MODE === "production" ? `${address}` : `${address}:${port}`;
+const url = import.meta.env.VITE_API_URL || 'http://0.0.0.0:3000';
 
 export const getEmployees = () => fetch(`${url}/employees`)
   .then((response) => response.json())
