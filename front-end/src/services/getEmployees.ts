6146466e9483f9592c212425af5df6eef2f90c6d
Carlos Antonio Sales de Oliveira Junior <carlosamtoni@gmail.com>
2024-07-26 15:55:44 -0300

    refactor: modificando a url da api para quando a aplicação estiver em produção

--- front-end/src/services/getEmployees.ts
+++ front-end/src/services/getEmployees.ts
@@ -1,6 +1,7 @@
 const address = import.meta.env.VITE_API_ADDRESS || 'http://0.0.0.0';
 const port = import.meta.env.VITE_API_PORT || '3000';
+const url =  import.meta.env.VITE_API_MODE === "production" ? `${address}` : `${address}:${port}`;
 
-export const getEmployees = () => fetch(`${address}:${port}/employees`)
+export const getEmployees = () => fetch(`${url}/employees`)
   .then((response) => response.json())
   .then((data) => data);
